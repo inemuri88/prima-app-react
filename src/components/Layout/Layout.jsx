7237a48f8f8bb617b283cf8b3b26bd5cae482358
Francesc Canzaniello <canzaniello.francesco1988@gmail.com>
2023-03-28 09:51:00 +0200

    prime parti di react

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar"
+
+function Layout() {
+    return (
+        <>
+        <p>sono il layout</p>
+        <Navbar></Navbar>
+        </>
+    )
+}
+
+export default Layout
